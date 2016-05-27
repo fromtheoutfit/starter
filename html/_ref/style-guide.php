216a6faa5052ef870160e4fa3705fd55112db26d
marcamos <marc@fromtheoutfit.com>
2016-05-27 12:47:02 -0400

    Generalized /_templates/ to /_ref/ and created style guide foundation.

---
+++ html/_ref/style-guide.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="/lib/css/all.css" />
+    <title>Style Guide</title>
+    <meta name="description" content="This project’s style guide" />
+  </head>
+  <body>
+
+    <h1>A work in progress!</h1>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="/lib/js/jquery.js"><\/script>')</script>
+    <script src="/lib/js/global.min.js"></script>
+
+    <!--
+    YOUR NON-GLOBAL, FEATURE-SPECIFIC JS FILE(S) HERE, LIKE SO:
+    <script src="/lib/js/neat-slider.min.js"></script>
+    <script src="/lib/js/great-angular-form-magic.min.js"></script>
+    <script src="/lib/js/dope-modal.min.js"></script>
+    -->
+
+  </body>
+</html>
