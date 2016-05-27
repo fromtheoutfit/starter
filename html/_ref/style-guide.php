959d681b416350af28bf8a07c4fd7048b3eb86a7
marcamos <marc@fromtheoutfit.com>
2016-05-27 13:02:14 -0400

    Style Guide doesn't need JS.

--- html/_ref/style-guide.php
+++ html/_ref/style-guide.php
@@ -12,16 +12,5 @@
 
     <h1>A work in progress!</h1>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="/lib/js/jquery.js"><\/script>')</script>
-    <script src="/lib/js/global.min.js"></script>
-
-    <!--
-    YOUR NON-GLOBAL, FEATURE-SPECIFIC JS FILE(S) HERE, LIKE SO:
-    <script src="/lib/js/neat-slider.min.js"></script>
-    <script src="/lib/js/great-angular-form-magic.min.js"></script>
-    <script src="/lib/js/dope-modal.min.js"></script>
-    -->
-
   </body>
 </html>
