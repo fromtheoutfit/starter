f63fd2fd9555a1369b62e9ca3a8c08e5734dbacd
marcamos <marc@theout.fit>
2017-06-20 14:11:17 -0400

    Minor improvements to the temporary /public/index document.

---
+++ public/index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+
+    <?php include_once '_demos/inc/head.php'; ?>
+
+    <!--
+    ( * NOTE * ) Most of <head>'s elements are stored in the include above. The
+    rest are below, as they're page-specific:
+    -->
+    <title>TODO: update this with a quality page title</title>
+    <meta name="description" content="TODO: update this with a quality page description" />
+
+    <!--
+    ( * OPTIONAL * ) FACEBOOK / TWITTER: Is this project utilizing Facebook and
+    Twitter? If so, utilize the code below and update the assets it references.
+    If not, delete the code below and the assets that it references.
+    - Facebook: https://developers.facebook.com/docs/sharing/best-practices
+    - Twitter:  https://dev.twitter.com/cards/getting-started
+    -->
+    <meta property="og:title" content="TODO: update this with a quality page title" />
+    <meta property="og:description" content="TODO: update this with a quality page description" />
+    <meta property="og:url" content="TODO: update this with an absolute URL to this page" />
+    <meta property="og:image" content="http://absolute-path.com/to/1200x630.jpg" />
+
+  </head>
+  <body>
+
+    <div class="copy-block" style="padding:50px 25px 25px;">
+
+      <h1>Hello!</h1>
+
+      <p>At the very begining of this project, you're probably building static templates that'll eventually be integrated into a dynamic back-end (CMS, Laravel, etc.) Assuming this is true, this very file (/public/index.php) is temporary and will soon be replaced by whatever the dynamic back-end requires. So, instead of building stuff <em>here</em>, consider the following:</p>
+
+      <ol>
+        <li>Look at the <a href="/_demos/">demos</a> to see what’s available</li>
+        <li>Build your static templates wherever necessary, based on the dynamic back-end of choice (ask Michael and/or Bren)</li>
+        <li>Treat yo' self</li>
+      </ol>
+
+    </div>
+
+  </body>
+</html>
