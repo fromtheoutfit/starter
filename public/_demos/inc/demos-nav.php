b89a1b6f7e77f33e8fddbc68285af05d0f7cbff2
marcamos <marc@theout.fit>
2017-06-20 13:15:51 -0400

    A bit of polish in Demo Nav.

--- public/_demos/inc/demos-nav.php
+++ public/_demos/inc/demos-nav.php
@@ -1,7 +1,7 @@
 <nav class="demos-nav js-demos-nav">
 
-  <!-- Single Pages -->
-  <h1>Individual Pages:</h1>
+  <!-- General -->
+  <h1>General:</h1>
   <div class="demos-nav__list">
     <a href="/_demos/bare-document.php">Bare Document</a>
     <a href="/_demos/dialog.php">Dialog</a>
@@ -27,12 +27,12 @@
   <h1>Layouts:</h1>
   <div class="demos-nav__list">
     <a href="/_demos/layout/plain.php">Plain grid</a>
-    <a href="/_demos/layout/gutters.php">Gutters</a>
+    <a href="/_demos/layout/gutters.php">Grid with gutters</a>
     <a href="/_demos/layout/equal-height-items.php">Equal height <code>module__item</code>s</a>
     <a href="/_demos/layout/item-wrapping.php"><code>module__item</code> wrapping</a>
-    <a href="/_demos/layout/item-width.php"><code>module__item</code> width</a>
-    <a href="/_demos/layout/item-width-changing-at-breakpoints.php"><code>module__item</code> width changing at breakpoints</a>
-    <a href="/_demos/layout/item-alignment.php"><code>module__item</code> alignment</a>
+    <a href="/_demos/layout/item-width.php"><code>module__item</code> width(s)</a>
+    <a href="/_demos/layout/item-width-changing-at-breakpoints.php"><code>module__item</code> width(s) changing at breakpoints</a>
+    <a href="/_demos/layout/item-alignment.php"><code>module__item</code> h/v alignment</a>
     <a href="/_demos/layout/nested-grids.php">Nested grids</a>
     <a href="/_demos/layout/row-order-reversal.php">Row order reversal</a>
   </div>
