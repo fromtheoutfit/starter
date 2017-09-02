aadfe26229d9b4abb7ad6c760281d6c3c60efa40
marcamos <marc@theout.fit>
2017-09-02 14:47:23 -0400

    Update Laravel Mix to v1.4.2

--- webpack.mix.js
+++ webpack.mix.js
@@ -44,6 +44,7 @@ mix.autoload({jquery: ['$', 'window.jQuery', 'jQuery']})
 // mix.fastSass('src', output); <-- Alias for mix.standaloneSass().
 // mix.less(src, output);
 // mix.stylus(src, output);
+// mix.postCss(src, output, [require('postcss-some-plugin')()]);
 // mix.browserSync('my-site.dev');
 // mix.combine(files, destination);
 // mix.babel(files, destination); <-- Identical to mix.combine(), but also includes Babel compilation.
