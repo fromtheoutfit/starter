6ab420f142a202c59447f95268a4cd0776b2bcef
marcamos <marc@theout.fit>
2018-02-16 16:14:46 -0500

    Add new url to import/export examples.

--- _js/app.js
+++ _js/app.js
@@ -1,4 +1,5 @@
 // Instructions on how to import other JS files into this one.
+// https://babeljs.io/learn-es2015/#modules
 // http://exploringjs.com/es6/ch_modules.html
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/export
