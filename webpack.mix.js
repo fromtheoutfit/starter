3218db1488e2657417ca8bb62aa14a2b6d7e2ea2
marcamos <marc@theout.fit>
2018-04-09 15:36:20 -0400

    Update Laravel Mix and most dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,5 @@
 let mix = require('laravel-mix')
-let glob = require('glob-all')
-let purgecss = require('purgecss-webpack-plugin')
+require('laravel-mix-purgecss')
 
 const config = {
   localUrl: 'starter.test',
@@ -32,12 +31,15 @@ mix
     'ally.js/ally.js': ['ally']
   })
   .js('_js/app.js', 'public/lib/js')
-  .sass('_scss/app.scss', 'public/lib/css')
-  .sass('_scss/print.scss', 'public/lib/css')
+  .standaloneSass('_scss/app.scss', 'public/lib/css')
+  .standaloneSass('_scss/print.scss', 'public/lib/css')
+  .sourceMaps()
+  .purgeCss({
+    globs: config.templates
+  })
   .options({
     processCssUrls: false
   })
-  .sourceMaps()
   .browserSync({
     proxy: config.localUrl,
     files: config.templates.concat([
@@ -47,21 +49,12 @@ mix
     ])
   })
 
-// In production only...
-if (mix.inProduction()) {
-  mix.webpackConfig({
-    plugins: [
-      // https://github.com/FullHuman/purgecss
-      new purgecss({
-        paths: glob.sync(config.templates)
-      })
-    ]
-  })
-}
-
 // Full API
 // mix.js(src, output);
 // mix.react(src, output); <-- Identical to mix.js(), but registers React Babel compilation.
+// mix.preact(src, output); <-- Identical to mix.js(), but registers Preact compilation.
+// mix.coffee(src, output); <-- Identical to mix.js(), but registers CoffeeScript compilation.
+// mix.ts(src, output); <-- TypeScript support. Requires tsconfig.json to exist in the same folder as webpack.mix.js
 // mix.extract(vendorLibs);
 // mix.sass(src, output);
 // mix.standaloneSass('src', output); <-- Faster, but isolated from Webpack.
@@ -69,7 +62,7 @@ if (mix.inProduction()) {
 // mix.less(src, output);
 // mix.stylus(src, output);
 // mix.postCss(src, output, [require('postcss-some-plugin')()]);
-// mix.browserSync('my-site.dev');
+// mix.browserSync('my-site.test');
 // mix.combine(files, destination);
 // mix.babel(files, destination); <-- Identical to mix.combine(), but also includes Babel compilation.
 // mix.copy(from, to);
@@ -82,9 +75,12 @@ if (mix.inProduction()) {
 // mix.setResourceRoot('prefix/for/resource/locators');
 // mix.autoload({}); <-- Will be passed to Webpack's ProvidePlugin.
 // mix.webpackConfig({}); <-- Override webpack.config.js, without editing the file directly.
+// mix.babelConfig({}); <-- Merge extra Babel configuration (plugins, etc.) with Mix's default.
 // mix.then(function () {}) <-- Will be triggered each time Webpack finishes building.
+// mix.extend(name, handler) <-- Extend Mix's API with your own components.
 // mix.options({
 //   extractVueStyles: false, // Extract .vue component styling to file, rather than inline.
+//   globalVueStyles: file, // Variables file to be imported in every component.
 //   processCssUrls: true, // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
 //   purifyCss: false, // Remove unused CSS selectors.
 //   uglify: {}, // Uglify-specific options. https://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
